e4f7e09e3f865af172ffc7251ef31b7eb249b8cb
Luiz Henrique de Figueiredo <>
2010-10-30 02:08:59 +0000

    Imported from 5.1/lgdbm.tar.gz.

--- lgdbm.c
+++ lgdbm.c
@@ -1,8 +1,8 @@
 /*
 * lgdbm.c
-* gdbm interface for Lua 5.0
+* gdbm interface for Lua 5.1
 * Luiz Henrique de Figueiredo <lhf@tecgraf.puc-rio.br>
-* 19 Jul 2006 14:22:44
+* 30 Oct 2010 00:02:05
 * This code is hereby placed in the public domain.
 */
 
@@ -16,14 +16,8 @@
 #include "lua.h"
 #include "lauxlib.h"
 
-#ifndef lua_boxpointer
-#define lua_boxpointer(L,u) \
-	(*(void **)(lua_newuserdata(L, sizeof(void *))) = (u))
-#define lua_unboxpointer(L,i)	(*(void **)(lua_touserdata(L, i)))
-#endif
-
 #define MYNAME		"gdbm"
-#define MYVERSION	MYNAME " library for " LUA_VERSION " / Jul 2006 / \n"
+#define MYVERSION	MYNAME " library for " LUA_VERSION " / Oct 2010 / \n"
 #define MYTYPE		MYNAME " handle"
 
 static datum encode(lua_State *L, int i)
@@ -59,9 +53,9 @@ static const char* errorstring(void)
 
 static int pushresult(lua_State *L, int rc)
 {
- if (rc>=0)
+ if (rc==0)
  {
-  lua_pushboolean(L,!rc);
+  lua_settop(L,1);
   return 1;
  }
  else
@@ -83,23 +77,16 @@ static GDBM_FILE Pget(lua_State *L, int i)
 {
  errno=0;
  LL=L;
- if (luaL_checkudata(L,i,MYTYPE)==NULL) luaL_typerror(L,i,MYTYPE);
- return lua_unboxpointer(L,i);
-}
-
-static GDBM_FILE Dget(lua_State *L, int i)
-{
- GDBM_FILE dbf=Pget(L,i);
- if (dbf==NULL) luaL_error(L,"invalid %s",MYTYPE);
- return dbf;
+ return *((void**)luaL_checkudata(L,i,MYTYPE));
 }
 
 static int Lopen(lua_State *L)			/** open(file,how) */
 {
  const char* file=luaL_checkstring(L,1);
- const char* how=luaL_checkstring(L,2);
+ const char* how=luaL_optstring(L,2,"r");
  int read_write;
  GDBM_FILE dbf;
+ GDBM_FILE* p=lua_newuserdata(L,sizeof(GDBM_FILE));
  switch (how[0])
  {
   default:
@@ -119,7 +106,7 @@ static int Lopen(lua_State *L)			/** open(file,how) */
  }
  else
  {
-  lua_boxpointer(L,dbf);
+  *p=dbf;
   luaL_getmetatable(L,MYTYPE);
   lua_setmetatable(L,-2);
   return 1;
@@ -128,30 +115,31 @@ static int Lopen(lua_State *L)			/** open(file,how) */
 
 static int Lclose(lua_State *L)			/** close(file) */
 {
- GDBM_FILE dbf=Dget(L,1);
- GDBM_FILE *p=lua_touserdata(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  gdbm_close(dbf);
- *p=NULL;
+ lua_pushnil(L);
+ lua_setmetatable(L,1);
  return 0;
 }
 
 static int Lreorganize(lua_State *L)		/** reorganize(file) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  int rc=gdbm_reorganize(dbf);
  return pushresult(L,rc);
 }
 
 static int Lsync(lua_State *L)			/** sync(file) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  gdbm_sync(dbf);
- return 0;
+ lua_settop(L,1);
+ return 1;
 }
 
 static int Lexists(lua_State *L)		/** exists(file,key) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  datum key=encode(L,2);
  int rc=gdbm_exists(dbf,key);
  lua_pushboolean(L,rc);
@@ -160,7 +148,7 @@ static int Lexists(lua_State *L)		/** exists(file,key) */
 
 static int Lfetch(lua_State *L)			/** fetch(file,key) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  datum key=encode(L,2);
  datum dat=gdbm_fetch(dbf,key);
  return decode(L,dat);
@@ -168,7 +156,7 @@ static int Lfetch(lua_State *L)			/** fetch(file,key) */
 
 static int Pstore(lua_State *L, int flags)
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  datum key=encode(L,2);
  datum dat=encode(L,3);
  int rc=gdbm_store(dbf,key,dat,flags);
@@ -187,7 +175,7 @@ static int Lreplace(lua_State *L)		/** replace(file,key,data) */
 
 static int Ldelete(lua_State *L)		/** delete(file,key) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  datum key=encode(L,2);
  int rc=gdbm_delete(dbf,key);
  return pushresult(L,rc);
@@ -195,54 +183,33 @@ static int Ldelete(lua_State *L)		/** delete(file,key) */
 
 static int Lfirstkey(lua_State *L)		/** firstkey(file) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  datum dat=gdbm_firstkey(dbf);
  return decode(L,dat);
 }
 
 static int Lnextkey(lua_State *L)		/** nextkey(file,key) */
 {
- GDBM_FILE dbf=Dget(L,1);
+ GDBM_FILE dbf=Pget(L,1);
  datum key=encode(L,2);
  datum dat=gdbm_nextkey(dbf,key);
  return decode(L,dat);
 }
 
-static int Lfdesc(lua_State *L)			/** fdesc(file) */
-{
-#ifdef GDBM_NOLOCK
- GDBM_FILE dbf=Dget(L,1);
- lua_pushnumber(L,gdbm_fdesc(dbf));
-#else
- lua_pushnil(L);
-#endif
- return 1;
-}
-
-static int Ltostring(lua_State *L)              /** tostring(file) */
+static int Ltostring(lua_State *L)
 {
  GDBM_FILE p=Pget(L,1);
- char s[64];
- sprintf(s,"%s %p (%p)",MYTYPE,(void*)p,lua_touserdata(L,1));
- lua_pushstring(L,s);
+ lua_pushfstring(L,"%s %p",MYTYPE,(void*)p);
  return 1;
 }
 
-static int Lgc(lua_State *L)
+static const luaL_Reg R[] =
 {
- GDBM_FILE dbf=Pget(L,1);
- if (dbf!=NULL) gdbm_close(dbf);
- return 0;
-}
-
-static const luaL_reg R[] =
-{
-	{ "__gc",	Lgc		},
-	{ "__tostring",	Ltostring	},
+	{ "__gc",	Lclose		},
+	{ "__tostring",	Ltostring	},	/** __tostring(file) */
 	{ "close",	Lclose		},
 	{ "delete",	Ldelete		},
 	{ "exists",	Lexists		},
-	{ "fdesc",	Lfdesc		},
 	{ "fetch",	Lfetch		},
 	{ "firstkey",	Lfirstkey	},
 	{ "insert",	Linsert		},
@@ -251,15 +218,14 @@ static const luaL_reg R[] =
 	{ "reorganize",	Lreorganize	},
 	{ "replace",	Lreplace	},
 	{ "sync",	Lsync		},
-	{ "tostring",	Ltostring	},
 	{ NULL,		NULL		}
 };
 
 LUALIB_API int luaopen_gdbm(lua_State *L)
 {
- lua_pushliteral(L,MYNAME);
  luaL_newmetatable(L,MYTYPE);
- luaL_openlib(L,NULL,R,0);
+ lua_setglobal(L,MYNAME);
+ luaL_register(L,MYNAME,R);
  lua_pushliteral(L,"version");			/** version */
  lua_pushliteral(L,MYVERSION);
  lua_pushstring(L,gdbm_version);
@@ -268,6 +234,5 @@ LUALIB_API int luaopen_gdbm(lua_State *L)
  lua_pushliteral(L,"__index");
  lua_pushvalue(L,-2);
  lua_settable(L,-3);
- lua_rawset(L,LUA_GLOBALSINDEX);
  return 1;
 }
--- test.lua
+++ test.lua
@@ -1,16 +1,58 @@
 -- test gdbm binding
 
+require"gdbm"
+
+-- support code --------------------------------------------------------------
+
+gdbm.version=string.gsub(gdbm.version,"\n.*version (.-). .*$","using GDBM %1")
+
+do
+local M= {
+	__index=function (t,k) return t.gdbm:fetch(k) end,
+	__newindex=function (t,k,v)
+		if v~=nil then t.gdbm:replace(k,v) else t.gdbm:delete(k) end
+	end
+}
+
+function gdbm.proxy(t)
+ return setmetatable({gdbm=t},M)
+end
+
+local function keys(d,k)
+  if k==nil then return d:firstkey() else return d:nextkey(k) end
+end
+
+function gdbm.keys(d)
+ return keys,d
+end
+
+local function entries(d,k)
+  local v
+  if k==nil then k=d:firstkey() else k=d:nextkey(k) end
+  if k==nil then v=nil else v=d:fetch(k) end
+  return k,v
+end
+
+function gdbm.entries(d)
+ return entries,d
+end
+
+end
+
+------------------------------------------------------------------------------
+
 function testing(s)
  print""
  print("-------------------------------------------------------",s)
 end
 
-function show(d)
+function gdbm.show(d)
  local n=0
  for k,v in d:entries() do
   n=n+1
-  print(n,k,v) 
+  print(n,k,v)
  end
+ return d
 end
 
 ------------------------------------------------------------------------------
@@ -27,8 +69,7 @@ d:insert("MAR","March")
 d:insert("APR","April")
 d:insert("MAY","May")
 d:insert("JUN","June")
-
-show(d)
+d:show()
 
 ------------------------------------------------------------------------------
 testing"proxy insert"
@@ -39,8 +80,7 @@ t.SEP="September"
 t.OCT="October"
 t.NOV="November"
 t.DEC="December"
-
-show(d)
+d:show()
 
 ------------------------------------------------------------------------------
 testing"proxy delete"
@@ -50,12 +90,21 @@ t.MAR=nil
 t.APR=nil
 t.MAY=nil
 t.JUN=nil
+d:show()
 
-show(d)
+--[[
+for i=1,1000 do t[i]=tostring(i) end
+os.execute("ls -l "..F.." ; md5sum "..F)
+for i=1,1000 do t[i]=nil end
+print("reorganize",d:reorganize())
+os.execute("ls -l "..F.." ; md5sum "..F)
+--]]
 
 ------------------------------------------------------------------------------
 testing"return codes"
 print("JAN exists?",d:exists("JAN"))
+print("replace JAN?",d:replace("JAN","Janeiro"))
+print("JAN is now",t.JAN)
 print("DEC exists?",d:exists("DEC"))
 print("replace DEC?",d:replace("DEC","Dezembro"))
 print("DEC is now",t.DEC)
@@ -73,12 +122,9 @@ print("XXX is",d:fetch("XXX"))
 print("delete XXX",d:delete("XXX"))
 print("delete XXX",d:delete("XXX"))
 
-testing"closing and gc"
-print(d)
-d:close()
-print(d)
-d=nil
-print(d)
+------------------------------------------------------------------------------
+testing"chaining"
+d:insert("FEB","Fevereiro"):delete("JUL"):replace("NOV","Novembro"):show():close()
 
 ------------------------------------------------------------------------------
 print""
